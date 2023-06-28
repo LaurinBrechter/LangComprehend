4c210c46fe3896b67cc97879267b0da507b92df3
laurinbrechter <brechterlaurin@gmail.com>
2023-06-28 15:03:29 +0200

    removed old node js backend

--- lc/backend/routes/router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-
-const textRouter = require('./new_text');
-// const baseRouter = require('./base');
-
-router.use("/", textRouter);
-
-module.exports = router;
