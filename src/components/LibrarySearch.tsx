587ae24f23a49e6979696aa3c9219001b9cd1bad
laurinbrechter <brechterlaurin@gmail.com>
2024-12-16 21:58:05 +0100

    remove comprehension section

--- src/components/LibrarySearch.tsx
+++ src/components/LibrarySearch.tsx
@@ -12,7 +12,7 @@ const LibrarySearch = ({ search }: { search?: string }) => {
     const router = useRouter()
 
     return (
-        <form className='bg-slate-300 p-4 flex gap-4 rounded-md' action={fetchWorksheets}>
+        <form className='p-4 flex gap-4 rounded-md border' action={fetchWorksheets}>
             <Input type="text" placeholder="Search" className="input input-bordered w-full max-w-xs" name='search' />
             <select className="select select-bordered w-full max-w-xs" defaultValue={"Language"} name='language'>
                 <option value={"de"}>German</option>
